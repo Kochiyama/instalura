336d1ae68e5cb4d3e13853ddb080ec4a01fffc78
Kochiyama <marcelokochiyama2210@gmail.com>
2020-09-18 02:03:42 -0300

    add image to post

--- App.js
+++ App.js
@@ -2,11 +2,15 @@ import { StatusBar } from "expo-status-bar";
 import React, { Fragment } from "react";
 import { StyleSheet, Text, Image } from "react-native";
 
+import image from "./res/alura.jpg";
+
 export default function App() {
   return (
     <Fragment>
       <Text>Marcelo</Text>
+      <Image source={image} />
       <Text>Diana</Text>
+      <Image source={image} />
       <StatusBar style="auto" />
     </Fragment>
   );
