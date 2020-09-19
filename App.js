f51819327bb95c45ba53370b9d7faa7081700476
Kochiyama <marcelokochiyama2210@gmail.com>
2020-09-19 03:22:09 -0300

    add more test posts

--- App.js
+++ App.js
@@ -47,6 +47,48 @@ const posts = [
         "https://images.unsplash.com/photo-1600184831467-29f64598f37e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
     },
   },
+  {
+    id: 3,
+    user: {
+      name: "Marcelo Haruo Kochiyama",
+      imageUrl: {
+        uri:
+          "https://avatars0.githubusercontent.com/u/26096036?s=460&u=646df6230922902d6e60ff5a4f0a6db98a63e7a0&v=4",
+      },
+    },
+    imageUrl: {
+      uri:
+        "https://avatars0.githubusercontent.com/u/26096036?s=460&u=646df6230922902d6e60ff5a4f0a6db98a63e7a0&v=4",
+    },
+  },
+  {
+    id: 4,
+    user: {
+      name: "Diana Priscila",
+      imageUrl: {
+        uri:
+          "https://avatars0.githubusercontent.com/u/26096036?s=460&u=646df6230922902d6e60ff5a4f0a6db98a63e7a0&v=4",
+      },
+    },
+    imageUrl: {
+      uri:
+        "https://images.unsplash.com/photo-1600348351012-59ce67d1ef8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
+    },
+  },
+  {
+    id: 5,
+    user: {
+      name: "Marcelo Haruo Kochiyama",
+      imageUrl: {
+        uri:
+          "https://avatars0.githubusercontent.com/u/26096036?s=460&u=646df6230922902d6e60ff5a4f0a6db98a63e7a0&v=4",
+      },
+    },
+    imageUrl: {
+      uri:
+        "https://images.unsplash.com/photo-1600184831467-29f64598f37e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+    },
+  },
 ];
 
 const deviceWidth = Dimensions.get("screen").width;
