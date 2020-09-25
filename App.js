87f8e29a4fae6e91b238372f4757bf3336a48e86
Kochiyama <marcelokochiyama2210@gmail.com>
2020-09-25 13:28:06 -0400

    moved dashboard, using new components

--- App.js
+++ App.js
@@ -1,40 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import React, { Fragment, useEffect, useState } from "react";
-import { FlatList } from "react-native";
+import React from "react";
+import Dashboard from "./src/views/Dashboard";
+import Login from "./src/views/Login";
 
-import getPostsFromApi from "./src/api/feed";
+const App = () => (
+  <>
+    <StatusBar style="auto" />
+    <Dashboard />
+  </>
+);
 
-import Header from "./src/components/Header/index";
-import Post from "./src/components/Post/index";
-
-export default function App() {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    getPostsFromApi(setPosts);
-    console.log(posts);
-  }, []);
-
-  const post = ({ item }) => (
-    <Post
-      userName={item.userName}
-      userUrl={item.userURL}
-      url={item.url}
-      description={item.description}
-      likes={item.likes}
-      comments={item.comentarios}
-    />
-  );
-
-  return (
-    <Fragment>
-      <StatusBar style="auto" />
-      <Header />
-      <FlatList
-        data={posts}
-        renderItem={post}
-        keyExtractor={(item) => item.id.toString()}
-      />
-    </Fragment>
-  );
-}
+export default App;
