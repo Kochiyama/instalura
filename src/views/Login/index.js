51c2d07aac0a7dbe59047fc8341db2441b86b485
Kochiyama <marcelokochiyama2210@gmail.com>
2020-09-25 13:27:47 -0400

    create a login component

---
+++ src/views/Login/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text } from "react-native";
+
+const Login = () => {
+  return (
+    <>
+      <Text>Login Page</Text>
+    </>
+  );
+};
+
+export default Login;
